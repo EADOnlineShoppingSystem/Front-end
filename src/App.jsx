e0e6203d218012854913c83d5a71b72fb5abaa69
Chathura <cpriydasun@gmail.com>
2024-11-16 00:23:38 +0530

    Category Page Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Components/HomePage/HomePage"
+import ProductCategories from "./Components/ProductCategories/Categories"
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/categories" element={<ProductCategories />} />
         </Routes>
       </BrowserRouter>
     </>
